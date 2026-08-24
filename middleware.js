01647c7a8879f81bbb5a1c3b18610b836b89c692
Anvi pardhi <anvipardhi10005@gmail.com>
2026-08-24 18:03:21 +0530

    Fix listing owner middleware

--- middleware.js
+++ middleware.js
@@ -1,62 +1,86 @@
-const Listing=require("./models/listing");
-const review = require("./models/review.js");
-const Review=require("./models/review.js");
-const { listingSchema, reviewSchema } =require("./schema");
-const ExpressError=require("./utils/expresserror.js");
+const Listing = require("./models/listing");
+const Review = require("./models/review.js");
+const { listingSchema, reviewSchema } = require("./schema");
+const ExpressError = require("./utils/expresserror.js");
 
-module.exports.isLoggedIn = (req,res,next) => {
-    if(!req.isAuthenticated()){
-        req.session.redirectUrl=req.originalUrl;
-        req.flash("error","you must be logged in to create a listing");
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "You must be logged in to create a listing");
         return res.redirect("/login");
     }
     next();
 };
 
-module.exports.saveRedirectUrl=(req,res,next)=>{
-    if(req.session.redirectUrl){
-        res.locals.redirectUrl=req.session.redirectUrl;
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
-}
+};
 
-
-module.exports.isOwner= async (req,res,next)=>{
+module.exports.isOwner = async (req, res, next) => {
     let { id } = req.params;
-    let listing=await Listing.findById(id);
-    if(!listing.owner.equals(res.locals.currUser._id)){
-        req.flash("error","you dont't have permission to edit");
+
+    const listing = await Listing.findById(id);
+
+    // Listing doesn't exist
+    if (!listing) {
+        req.flash("error", "Listing doesn't exist");
+        return res.redirect("/listing");
+    }
+
+    // Listing has no owner
+    if (!listing.owner) {
+        req.flash("error", "This listing does not have an owner");
+        return res.redirect("/listing");
+    }
+
+    // Current user is not the owner
+    if (!listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "You don't have permission to edit this listing");
         return res.redirect(`/listing/${id}`);
     }
+
     next();
-}
+};
 
-module.exports.validatelisting=(req,res,next)=>{
-    let {error}= listingSchema.validate(req.body);
-    if(error){
-        let errmsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errmsg);
-    }else{
+module.exports.validatelisting = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        let errmsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errmsg);
+    } else {
         next();
     }
 };
 
-module.exports.validateReview=(req,res,next)=>{
-    let {error}= reviewSchema.validate(req.body);
-    if(error){
-        let errmsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errmsg);
-    }else{
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+
+    if (error) {
+        let errmsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errmsg);
+    } else {
         next();
     }
 };
 
-module.exports.isReviewAuthor= async (req,res,next)=>{
+module.exports.isReviewAuthor = async (req, res, next) => {
     let { id, reviewId } = req.params;
-    let review=await Review.findById(reviewId);
-    if(!review.author.equals(res.locals.currUser._id)){
-        req.flash("error","you dont't have permission to delete the review");
+
+    const review = await Review.findById(reviewId);
+
+    if (!review) {
+        req.flash("error", "Review doesn't exist");
         return res.redirect(`/listing/${id}`);
     }
+
+    if (!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You don't have permission to delete the review");
+        return res.redirect(`/listing/${id}`);
+    }
+
     next();
-}
+};
